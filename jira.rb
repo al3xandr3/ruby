d8d16375fdbf5f8e453a81ccf865072cb85cc3c4
Alexandre Martins <al3xandr3@gmail.com>
2013-06-05 10:32:43 +0300

    ruby

--- jira.rb
+++ jira.rb
@@ -6,12 +6,12 @@ require 'json'
 user = "user"
 pass = "pass"
 
-# "curl -D- -u USER:PASS -X GET -H "Content-Type: application/json" "https://jira.skype.net/rest/api/latest/search?jql=assignee%3D'some.user'"
+# "curl -D- -u USER:PASS -X GET -H "Content-Type: application/json" "https://jira.domain.net/rest/api/latest/search?jql=assignee%3D'some.user'"
 JQL = CGI.escape "assignee='some.user'"
 
-resp = %x{ curl -u #{user}:#{pass} -X GET -H "Content-Type: application/json" "https://jira.skype.net/rest/api/latest/search?jql=#{JQL}" }
+resp = %x{ curl -u #{user}:#{pass} -X GET -H "Content-Type: application/json" "https://jira.domain.net/rest/api/latest/search?jql=#{JQL}" }
 
 puts JSON.parse(resp)
 
 # OR from any page do: Views -> XML
-# curl -u USER:PASS -X GET "https://jira.skype.net/sr/jira.issueviews:searchrequest-xml/23211/SearchRequest-23211.xml?tempMax=1000"
+# curl -u USER:PASS -X GET "https://jira.domain.net/sr/jira.issueviews:searchrequest-xml/23211/SearchRequest-23211.xml?tempMax=1000"
